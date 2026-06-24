5a7b9ba67ac0cf6f05ab3569a40fdae3657239cc
lulu103 <andylan.mobile@gmail.com>
2026-06-24 09:12:31 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,23 +1,35 @@
-const cacheName="clock-v1";
+const CACHE_NAME = "clock-app-v2";
 
-self.addEventListener("install",(e)=>{
-
-e.waitUntil(
-caches.open(cacheName)
-.then(cache=>cache.addAll([
+const FILES = [
 "./",
 "./index.html",
-"./manifest.json"
-]))
-);
+"./manifest.json",
+"./icon-192.png",
+"./icon-512.png"
+];
 
+self.addEventListener("install", event => {
+event.waitUntil(
+caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+);
+self.skipWaiting();
 });
 
-self.addEventListener("fetch",(e)=>{
-
-e.respondWith(
-caches.match(e.request)
-.then(response=>response || fetch(e.request))
+self.addEventListener("activate", event => {
+event.waitUntil(
+caches.keys().then(keys =>
+Promise.all(keys.map(k => {
+if(k !== CACHE_NAME){
+return caches.delete(k);
+}
+}))
+)
 );
+self.clients.claim();
+});
 
+self.addEventListener("fetch", event => {
+event.respondWith(
+caches.match(event.request).then(res => res || fetch(event.request))
+);
 });
